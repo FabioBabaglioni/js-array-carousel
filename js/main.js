2ff6499cbeb6b5f50d3cec7e372bd2a6d07aa418
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-11 17:10:09 +0100

    inizio logica

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+let img = document.getElementsByClassName("items")
+
+console.log(img)
+
+let activeItem = 0
+
+const next = document.getElementById("next")
+const previous = document.getElementById("previous")
+
+
+next.addEventListener("click",
+
+    function(){
+
+    if(activeItem < img.length - 1){
+
+        img[activeItem].classList.remove("active")
+
+        activeItem++
+
+        img[activeItem].classList.add("active")
+
+        if(activeItem === img.length - 1){
+
+            next.classList.add("hidden")
+        }
+    }
+}
+)
+
+previous.addEventListener("click",
+
+    function(){
+
+    if(activeItem < img.length ){
+
+    }
+}
+)
